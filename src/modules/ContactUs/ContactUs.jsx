13e313c8fa0064be8d42d41292dc2ac85be1ea19
Valeryia <mylifeasval@gmail.com>
2022-11-30 16:11:25 +0300

    finalе

--- src/modules/ContactUs/ContactUs.jsx
+++ src/modules/ContactUs/ContactUs.jsx
@@ -1,39 +1,4 @@
-import { Form, Input } from 'antd';
-import { Modal2} from "../../components/Modal2";
-import { useState } from "react";
-
-export const ContactUs = () => {
-
-    const layout = {
-        wrapperCol: {
-          span: 7,
-        },
-    };
-    
-    const validateMessages = {
-        required: '${label} is required!',
-        types: {
-          email: '${label} is not a valid email!'
-        }
-    };
-    
-    const onFinish = (values) => {
-        console.log(values);
-    };
-
-    const [visible, setVisible] = useState(false);
-
-    const openModal = () => {
-        setVisible(true);
-        form.resetFields();
-    }
-
-    const handleModalClose = () => {
-        setVisible(false);
-    }
-
-    const [form] = Form.useForm();
-    
+export const ContactUs = () => {    
 
     return (        
         <div className="container">
@@ -63,41 +28,29 @@ export const ContactUs = () => {
                 </div>
                 <div className="contacts__form">
                     <h3 className="contacts__form__content">If you have any questions please fill out this contact form. Thank you!</h3>
-                    <Form {...layout} form={form} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-                        <Form.Item className='contacts__form__item'
-                            name={['user', 'name']}
-                            label="Your name"
-                            tooltip="What do you want others to call you?"
-                        >
-                            <Input className='contacts__form__input'/>
-                        </Form.Item>
-                        <Form.Item className='contacts__form__item'
-                            name={['user', 'email']}
-                            label="Your email"
-                            rules={[
-                            {
-                                type: 'email',
-                                required: true 
-                            },
-                            ]}
-                        >
-                            <Input className='contacts__form__input'/>
-                        </Form.Item>
-                        <Form.Item className='contacts__form__item' name={['user', 'question']} label="Your question" rules={[
-                            {
-                                required: true 
-                            },
-                            ]}>
-                            <Input.TextArea className='contacts__form__input'/>
-                        </Form.Item>
-                        <Form.Item
-                        >
-                            <button className='contacts__form__btn' htmltype="submit" onClick={openModal}> Submit </button>
-                            {visible && <Modal2 onClose={handleModalClose} content={<p className="modalContacts__text">Your message was successfully sent! We will reply to you shortly.</p>} button={<span>OK</span>}/>} 
-                        </Form.Item>
-                        </Form>
+                    <form>
+                        <div className="form__wrapper">
+                            <div>
+                                <p className="form__info">Your name </p>
+                                <input className='contacts__form__input'  name={['Name']}/>
+                            </div>
 
-                </div>
+                            <div>
+                                <p className="form__info">Your email</p>
+                                <input className='contacts__form__input' type={'email'} required name={["Email"]} />
+                            </div>
+
+                            <div>
+                                <p className="form__info">Your question<i title="Type any of your q-ns or concerns here.">?</i></p>
+                                <textarea className='contacts__form__input' name={['Introduction']} required />    
+                            </div>                    
+                               
+                            <button className="contacts__form__btn" type="Submit">Send</button> 
+
+                        </div>
+                    </form>
+                </div>                   
+                
             </div>
         </div>      
     )
